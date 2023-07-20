7d2e867817a56ea928705f3e41318e38964efae0
Imam Sutono <imamsutono>
2023-07-20 09:23:01 +0700

    add python code

---
+++ naive_bayes.py
@@ -0,0 +1,17 @@
+data = [
+  "Laki-laki", 20, 8000000, "Single", "Kendaraan pribadi",
+  "Laki-laki", 35, 14000000, "Single", "Kendaraan umum",
+  "Perempuan", 26, 10000000, "Single", "Kendaraan umum",
+  "Perempuan", 27, 12000000, "Menikah", "Kendaraan pribadi",
+  "Laki-laki", 21, 9000000, "Single", "Kendaraan pribadi",
+  "Laki-laki", 22, 11000000, "Single", "Kendaraan pribadi",
+  "Perempuan", 32, 15000000, "Menikah", "Kendaraan umum",
+  "Perempuan", 26, 8000000, "Menikah", "Kendaraan umum",
+  "Laki-laki", 25, 9000000, "Single", "Kendaraan umum",
+  "Perempuan", 20, 10000000, "Single", "Kendaraan pribadi"
+]
+
+datatest = [
+  "Perempuan", 27, 12000000, "Single", "?",
+  "Laki-laki", 35, 14000000, "Menikah", "?"
+]
